478f31326c44ba17dfa83ba7c2bad1cddfe3523c
Aman <120547566+Aman-008@users.noreply.github.com>
2024-11-01 15:43:44 +0530

    commited

--- basic.cpp
+++ basic.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    cout << n << endl;
+    // string n;
+    // cin >> n;
+    // cout << n << endl;
+    cout << "Hello World";
     return 0;
 }
