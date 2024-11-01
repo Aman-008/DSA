bfb6e285342a8bd089ecd3ec9003896f43917b59
Aman <120547566+Aman-008@users.noreply.github.com>
2024-11-01 18:07:27 +0530

    commited

--- basic.cpp
+++ basic.cpp
@@ -3,9 +3,143 @@ using namespace std;
 
 int main()
 {
-    // string n;
+    // range --> (-10^9 , 10^9)
+    // int numInt = INT_MAX;
+    // int numInt = INT_MIN;
+    // cout << numInt << endl;
+
+
+    // long long numLong = LONG_LONG_MAX;
+    // cout << numLong << endl;
+
+
+
+    // int bdate , num;
+    // cin >> bdate >> num;
+    // cout << bdate << " " << num << endl;
+    // cout << bdate << endl << num << endl;
+
+
+
+    // string str1 , str2;
+    // getline(cin,str1);
+    // getline(cin,str2);
+    // cout << str1 << endl << str2;
+    // cout << str1 << endl;
+    // cout << str2 << endl;
+
+
+
+
+    // if else
+    // int age;
+    // cin >> age;
+    // if (age >= 18){
+    //     cout << "Adult" << endl;
+    // }
+    // // else if (age < 18 and age >= 10)
+    // else if (age < 18 && age >= 10)
+    // {
+    //     cout << "Teen" << endl;
+    // }
+    
+    // else{
+    //     cout << "Child" << endl;
+    // }
+
+
+
+    // int marks;
+    // cin >> marks;
+    // if(marks >= 90){
+    //     cout << "A" << endl;
+    // }
+    // else if(marks >= 70){
+    //     cout << "B";
+    // }
+    // else if(marks >= 50){
+    //     cout << "C";
+    // }
+    // else if (marks >= 35){
+    //     cout << "D"; 
+    // }
+    // else{
+    //     cout << "Fail" << endl;
+    // }
+
+
+
+
+
+
+
+
+
+    // Switch Case
+
+    // int day;
+    // cin >> day;
+    // switch (day)
+    // {
+    // case 1:
+    //     cout << "Monday";
+    //     break;
+    // case 2:
+    //     cout << "Tuesday";
+    //     break;
+    // case 3:
+    //     cout << "Wednesday";
+    //     break;
+    // case 4:
+    //     cout << "Thursday";
+    //     break;
+    // case 5:
+    //     cout << "Friday";
+    //     break;
+    // case 6:
+    //     cout << "Saturday";
+    //     break;
+    // case 7:
+    //     cout << "Sunday";
+    //     break;
+    // default:
+    //     cout << "Invalid Number";
+    //     break;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+    // for loop
+
+    // for (int i=1;i<=10;i++){
+    //     cout << i << endl;
+    // }
+
+    // for (int i=10;i>=1;i--){
+    //     cout << i << endl;
+    // }
+
+    // int n;
     // cin >> n;
-    // cout << n << endl;
-    cout << "Hello World";
+    // for(int i=5;i<=n;i+=5){
+    //     cout << i << endl;
+    // }
+
+    // While Loop
+
+    int i = 5;
+    while(i<=100){
+        cout << i << endl;
+        i+=5;
+    }
     return 0;
 }
